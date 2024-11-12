1128260c9a1bb2fdce41fdcb2504877a6635d320
bassam <alsaffar.bassam@gmail.com>
2024-11-12 13:15:23 +0100

    task2

---
+++ bassam-task2.js
@@ -0,0 +1,5 @@
+const questions = [
+    new Question("What is the capital of France?", ["Berlin", "Madrid", "Paris", "Lisbon"], 3),
+    new Question("Which planet is known as the Red Planet?", ["Earth", "Mars", "Jupiter", "Saturn"], 2),
+    new Question("What is the largest ocean on Earth?", ["Atlantic", "Indian", "Arctic", "Pacific"], 4)
+];
