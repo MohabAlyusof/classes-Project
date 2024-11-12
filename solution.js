ac8668963f47d11aaa5c3432dae21a3a73a43169
Mohab Alyusof <Mohabalusef-1997@hotmail.com>
2024-11-12 13:27:11 +0100

    New file

---
+++ solution.js
@@ -0,0 +1,6 @@
+//### Task 1: Create a class named Question to represent a quiz question. The class should have the following features:
+
+//- A constructor that accepts the question text and an array of options.
+//- A method to display the question and its options.
+//- A method to check if the user's choice is correct.
+
